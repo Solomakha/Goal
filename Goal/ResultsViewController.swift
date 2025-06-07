298fcbc8153345ac44003112c524272e000af260
Solomakha <solomakhadmitry@gmail.com>
2025-06-07 16:37:49 +0300

    Created ResultsViewController.

---
+++ Goal/ResultsViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+       
+    }
+    
+}
