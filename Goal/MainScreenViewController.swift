2b3e8dacd0d218427de8b5827327bcd1b5fd4b57
Solomakha <solomakhadmitry@gmail.com>
2025-06-07 16:37:21 +0300

    Created MainScreenViewController.

---
+++ Goal/MainScreenViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class MainScreenViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+      
+    }
+
+}
